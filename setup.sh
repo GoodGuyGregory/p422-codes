0ddb6eebf019fda012f5c7950d280d66c3f879b9
GoodGuyGregory <greg.witt625@gmail.com>
2020-08-28 16:12:56 -0400

    adds wokring homebrew check

--- setup.sh
+++ setup.sh
@@ -10,55 +10,62 @@ function installBrew() {
 # HOMEBREW CHECK:
 
 #  checks for hombrew on host machine
-# if [ !$(which brew) ] ; then
-#  echo "installing homebrew on your system...."
-#  echo "trust me you will thank me later..."
-#  installBrew
-# fi 
-
+if [[ -f !$(which brew) ]]; then
+ echo "installing homebrew on your system...."
+ echo "trust me you will thank me later..."
+ installBrew
+fi 
 
 # SPECIFIED COURSE DEPENDENCIES:
-# CASKS="visual-studio-code"
+declare -a CASKS 
 FORMULAE="git node npm"
 
-echo "installing dev dependencies for p422....."
-echo " for this course you will need a text editor"
+echo "installing dev dependencies for P422....."
+echo "for this course you will need a text editor"
 
 # OPTIONAL TEXT EDITOR INSTALLATION:
 
-# read -p "do you want to install visual studio code? Y/N " ANSWER
-# case "$ANSWER" in 
-#     [yY] | [yY][eE][sS])
-#     CASKS=${visual-studio-code}
-#     ;;
-#     [nN] | [nN][oO])
-#     echo "You need a text editor"
-#     ;;
-# *)
-# # if they dont put anything
-#     echo "Please enter y/yes or n/no"
-#     ;;
-# esac
+read -p "do you want to install visual studio code? Y/N " ANSWER
+case "$ANSWER" in 
+    [yY] | [yY][eE][sS])
+    CASKS+=("visual-studio-code")
+    ;;
+    [nN] | [nN][oO])
+    echo "You need a text editor for this course"
+    ;;
+*)
+# if they dont put anything
+    echo "Please enter y/yes or n/no"
+    ;;
+esac
 
-# read -p "do you want to install webstorm? Y/N " ANSWER
-# case "$ANSWER" in 
-#     [yY] | [yY][eE][sS])
-#     CASKS=${webstorm}
-#     ;;
-#     [nN] | [nN][oO])
-#     echo "You need a text editor"
-#     ;;
-# *)
-# # if they dont put anything
-#     echo "Please enter y/yes or n/no"
-#     ;;
-# esac
+read -p "do you want to install webstorm? Y/N " ANSWER
+case "$ANSWER" in [yY] | [yY][eE][sS])
+        echo "ensure you are registered with your Jetbrains Student pack..."
+        echo "opening jetbrains registration site..."
+        sleep 5 
+        open https://account.jetbrains.com/login
+        CASKS+=("webstorm")
+    ;;
+    [nN] | [nN][oO])
+    echo "You need to download a text editor for this course"
+    ;;
+*)
+# if they dont put anything
+    echo "Please enter y/yes or n/no"
+    ;;
+esac
 
+for cask in $CASKS
+    do
+        echo "installing $cask"
+        # brew cask install "$CASKS"
+    done
 
 for formulae in $FORMULAE
     do
+    echo "installing $formulae"
         # brew install "$formulae"
-        echo "$formulae"
     done
 
 #  INSTALLING MONGODB
