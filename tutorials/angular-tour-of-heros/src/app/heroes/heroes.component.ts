844335a45583f7267ffa5f265ad87b8e225d21bc
GoodGuyGregory <greg.witt625@gmail.com>
2020-08-31 13:58:53 -0400

    adds messages and heroservice to app

--- tutorials/angular-tour-of-heros/src/app/heroes/heroes.component.ts
+++ tutorials/angular-tour-of-heros/src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,7 +13,7 @@ export class HeroesComponent implements OnInit {
   //   id: 1,
   //   name: 'Windstorm'
   // };
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
@@ -22,9 +22,17 @@ export class HeroesComponent implements OnInit {
   }
 
   
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
   ngOnInit(): void {
+    //  renders the heroes from the service
+    this.getHeroes();
   }
+
 
 }
