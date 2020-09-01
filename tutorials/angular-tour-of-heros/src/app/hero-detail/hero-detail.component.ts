60353e597d07a8629876664e6b27238eca2cd4b9
GoodGuyGregory <greg.witt625@gmail.com>
2020-09-01 19:59:13 -0400

    completes section 5

--- tutorials/angular-tour-of-heros/src/app/hero-detail/hero-detail.component.ts
+++ tutorials/angular-tour-of-heros/src/app/hero-detail/hero-detail.component.ts
@@ -1,11 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
-
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { HeroService} from '../hero.service';
 
-
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -13,25 +11,21 @@ import { HeroService} from '../hero.service';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
+  hero: Hero;
 
-  constructor( 
+  constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ){}
+  ) { }
 
   ngOnInit(): void {
     this.getHero();
   }
-//  recieves a hero object from the imported hero component
-  @Input() hero: Hero;
 
   getHero(): void {
-    //  gets the hero id from the router
-    // '+' converts the string returned to a number for retrieval
     const id = +this.route.snapshot.paramMap.get('id');
-    //  passes the id into the heroService to retrieve that hero 
-    this.heroService.getHeroes(id)
+    this.heroService.getHero(id)
       .subscribe(hero => this.hero = hero);
   }
 
