b37ae36f09c1374af4e45c7c1b8e8ce2825607e2
GoodGuyGregory <greg.witt625@gmail.com>
2020-09-02 22:20:53 -0400

    passes todos to todo-item component

--- tutorials/angular-crash-course/angular-crash-todolist/src/app/components/todo-item/todo-item.component.ts
+++ tutorials/angular-crash-course/angular-crash-todolist/src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+// adds input because we are passing the todos in from another component
+import { Component, OnInit, Input} from '@angular/core';
+import { Todo } from 'src/app/models/Todo';
 
 @Component({
   selector: 'app-todo-item',
@@ -6,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-item.component.css']
 })
 export class TodoItemComponent implements OnInit {
-
+  //  Input Properties will take anything passed to the component
+  @Input() todo: Todo;
+  
   constructor() { }
 
   ngOnInit(): void {
