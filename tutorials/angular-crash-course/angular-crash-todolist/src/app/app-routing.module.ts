a4d5ebb326e6f210816d35fd39a44a0e3987b586
GoodGuyGregory <greg.witt625@gmail.com>
2020-09-21 11:39:00 -0400

    adds Router components and completes tutorial

--- tutorials/angular-crash-course/angular-crash-todolist/src/app/app-routing.module.ts
+++ tutorials/angular-crash-course/angular-crash-todolist/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+//  To add Routes they must be imported into the Router
+import { TodosComponent } from './components/todos/todos.component'
+import { AboutComponent } from './components/pages/about/about.component'
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '', component: TodosComponent
+  },
+  {
+    path: 'about', component: AboutComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
