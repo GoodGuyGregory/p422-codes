e7954781a7d8c7c152080da159571eb273003cad
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-09 21:53:32 -0500

    adds profile models and static data

---
+++ lectures/profile/src/app/models/contactinfo.ts
@@ -0,0 +1,6 @@
+export class ContactInfo {
+    type: string;
+    phone: string;
+    address: string;
+    email: string;
+}
---
+++ lectures/profile/src/app/models/profile.ts
@@ -0,0 +1,7 @@
+import { ContactInfo } from './contactinfo';
+
+export class Profile {
+    name: string;
+    age: number;
+    contactinfo: ContactInfo[];
+}
