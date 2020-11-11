307a78fd0a600da6ccd8c507a8a1f953250600ba
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-10 19:10:38 -0500

    adds input decorator and table for users

--- lectures/angular-introduction/profile/src/app/profile/profile.component.ts
+++ lectures/angular-introduction/profile/src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Profile } from '../models/profile';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+
+  // adds input decorator to allow Profiles to be passed to this component 
+  @Input() profile: Profile;
 
   constructor() { }
 
