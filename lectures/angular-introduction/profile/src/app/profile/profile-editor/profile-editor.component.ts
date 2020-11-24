e2559f145b73ccdd6757e976366ed1c12821e03b
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-24 11:18:57 -0500

    adds interactive forms for client validation

--- lectures/angular-introduction/profile/src/app/profile/profile-editor/profile-editor.component.ts
+++ lectures/angular-introduction/profile/src/app/profile/profile-editor/profile-editor.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Profile } from 'src/app/models/profile';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
@@ -14,11 +15,11 @@ export class ProfileEditorComponent implements OnInit {
 
   // sets up form controls in angular
   person = new FormGroup({
-    name: new FormControl(),
-    age: new FormControl(),
-    phone: new FormControl(),
-    address: new FormControl(),
-    email: new FormControl(),
+    name: new FormControl('', [Validators.required]),
+    age: new FormControl('', [Validators.required, Validators.min(13), Validators.max(120)]),
+    phone: new FormControl('', [Validators.required, Validators.pattern('^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$')]),
+    address: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
   });
 
   constructor() { }
@@ -28,11 +29,15 @@ export class ProfileEditorComponent implements OnInit {
   }
 
   save(): void {
-    this.profile.name = this.person.value.name;
-    this.profile.age = this.person.value.age;
-    this.profile.phone = this.person.value.phone;
-    this.profile.address = this.person.value.address;
-    this.profile.email = this.person.value.email;
+    // Checks Validity of Validators
+    if (this.person.valid) {
+      this.profile.name = this.person.value.name;
+      this.profile.age = this.person.value.age;
+      this.profile.phone = this.person.value.phone;
+      this.profile.address = this.person.value.address;
+      this.profile.email = this.person.value.email;
+    }
+
 
     // sends an output to other components
     this.finishedEditing.emit(true);
