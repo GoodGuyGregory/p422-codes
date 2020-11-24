b7d9e6ef920084a7d461ba3c37e7d6f0410a53b3
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-24 15:09:47 -0500

    creates app and initial components needed

---
+++ lectures/angular-services-routing/courseware/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SyllabusComponent } from './app/components/syllabus/syllabus.component';
+import { AssignmentsComponent } from './components/assignments/assignments.component';
+import { AssignmentDetailComponent } from './components/assignment-detail/assignment-detail.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SyllabusComponent,
+    AssignmentsComponent,
+    AssignmentDetailComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
