505eccce9695f71e33638313acebc915d3f335b8
GoodGuyGregory <greg.witt625@gmail.com>
2021-11-11 09:13:57 -0500

    adds tasks service http and angular material

--- tutorials/angular-crash/src/app/app.module.ts
+++ tutorials/angular-crash/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { ButtonComponent } from './components/button/button.component';
 import { CommonModule } from '@angular/common';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,8 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
     BrowserModule,
     AppRoutingModule,
     CommonModule,
+    HttpClientModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- tutorials/angular-crash/src/app/services/task.service.ts
+++ tutorials/angular-crash/src/app/services/task.service.ts
@@ -2,16 +2,22 @@ import { Injectable } from '@angular/core';
 import { Task } from '../Task';
 import { TASKS } from '../mock-task';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  // hit the API URL:
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) { }
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    // const tasks = of(TASKS);
+    // return tasks;
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
